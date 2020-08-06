49782a71c0292da41885586dbd87a4d5eaad5b0b
Yehong Piao <piaoyehong1107@gmail.com>
2020-08-06 04:51:17 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -8,7 +8,23 @@ class PigLatinizer
   def piglatinize
      words = @string.split(" ")
      words.each do |m|
-       m.scan(/[aeoui]/)
+       m.scan(/[aeoui]/)words = string.split(" ")
+words.each do |m|
+    vowels=m.scan(/[aeoui]/)
+    vowindex=m.index(vowels[0])
+    n=m.split("")
+      if vowindex==0
+        pigword=n.push(["w","a","y"]).join
+      else
+          i=0
+          while i<vowindex do
+            a=n.shift
+            pigword=n.push(a).join
+          i+=1
+          end
+      end
+      puts pigword
+end
      end
   end
 
